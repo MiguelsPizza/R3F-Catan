a70cf86893bbfb697bf657d0c5b886b87723ba32
MiguelsPizza <alexmnahas@gmail.com>
2022-06-11 00:03:33 -0400

    attempted map randomizer

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,7 +5,7 @@ import { calculateBoardPositions } from "../helperFunctions/BoardSetupFunctions"
 
 function Board() {
   const [tiles, setTiles] = useState([]);
-  const { size, color } = useControls({ size: 45, color: "#4acf59" });
+  const { size } = useControls({ size: 45 });
 
   const mappedPositions = useMemo(() => calculateBoardPositions(size), [size]);
 
@@ -15,9 +15,24 @@ function Board() {
 
   return (
     <>
-      {tiles.map((piece, i) => (
-        <WoodsTile key={i} position={piece} color={color} />
-      ))}
+      {tiles.map((piece, i) => {// for some reason the color for all will be the last one. Not sure how to fix this yet
+        console.log(piece);
+        if (piece.tileType === "woods") {
+          return <WoodsTile key={i} position={piece.position} color="green" />;
+        } else if (piece.tileType === "ore") {
+          return <WoodsTile key={i} position={piece.position} color="grey" />;
+        } else if (piece.tileType === "stone") {
+          return <WoodsTile key={i} position={piece.position} color="brown" />;
+        } else if (piece.tileType === "sheep") {
+          return <WoodsTile key={i} position={piece.position} color="white" />;
+        } else if (piece.tileType === "wheat") {
+          return <WoodsTile key={i} position={piece.position} color="yellow" />;
+        } else {
+          return (
+            <WoodsTile key={i} position={piece.position} color="#f5f5f5" />
+          );
+        }
+      })}
     </>
   );
 }
--- src/helperFunctions/BoardSetupFunctions.js
+++ src/helperFunctions/BoardSetupFunctions.js
@@ -1,12 +1,19 @@
+
+
 const calculateBoardPositions = (size) => {
+  const tileTypes = ['sheep', 'sheep','sheep', 'sheep', 'woods', 'woods', 'woods', 'woods', 'wheat', 'wheat', 'wheat', 'wheat', 'brick', 'brick', 'brick', 'ore', 'ore', 'ore', 'desert'];
+
   const positions = mockApiResponseForBoardPositions();
   const mappedPositions = positions.map((pos) => {
     const [q, r] = pos;
-    return [
+    const position = [
       ((q * 3) / 2) * size,
       0,
       size * ((Math.sqrt(3) / 2) * q + Math.sqrt(3) * r),
     ];
+    const tileType = tileTypes[Math.floor(Math.random() * tileTypes.length)];
+
+    return {position, tileType};
   });
   return mappedPositions;
 };
