b81bb6e3657d4ca557082007c4aaaa04a524fc15
MiguelsPizza <alexmnahas@gmail.com>
2022-06-11 00:07:24 -0400

    changed to switch Case

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -15,22 +15,34 @@ function Board() {
 
   return (
     <>
-      {tiles.map((piece, i) => {// for some reason the color for all will be the last one. Not sure how to fix this yet
-        console.log(piece);
-        if (piece.tileType === "woods") {
-          return <WoodsTile key={i} position={piece.position} color="green" />;
-        } else if (piece.tileType === "ore") {
-          return <WoodsTile key={i} position={piece.position} color="grey" />;
-        } else if (piece.tileType === "stone") {
-          return <WoodsTile key={i} position={piece.position} color="brown" />;
-        } else if (piece.tileType === "sheep") {
-          return <WoodsTile key={i} position={piece.position} color="white" />;
-        } else if (piece.tileType === "wheat") {
-          return <WoodsTile key={i} position={piece.position} color="yellow" />;
-        } else {
-          return (
-            <WoodsTile key={i} position={piece.position} color="#f5f5f5" />
-          );
+      {tiles.map((piece, i) => {
+        switch (piece.tileType) {
+          case "woods":
+            return (
+              <WoodsTile key={i} position={piece.position} color="green" />
+            );
+          case "ore":
+            return (
+            <WoodsTile key={i} position={piece.position} color="grey" />
+            );
+          case "brick":
+            return (
+              <WoodsTile key={i} position={piece.position} color="brown" />
+            );
+          case "sheep":
+            return (
+              <WoodsTile key={i} position={piece.position} color="white" />
+            );
+          case "wheat":
+            return (
+              <WoodsTile key={i} position={piece.position} color="yellow" />
+            );
+          case "desert":
+            return (
+              <WoodsTile key={i} position={piece.position} color="#f5f5f5" />
+            );
+          default:
+            return null;
         }
       })}
     </>
